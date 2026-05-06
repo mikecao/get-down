68a40fc80548075c8b6bd960e54b90f60eca8ea2
Mike Cao <mike@mikecao.com>
2026-05-06 15:59:02 -0700

    Remove border radius from Checkbox to make corners square

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
   <BaseCheckbox.Root
     ref={ref}
     className={cn(
-      'peer grid h-4 w-4 shrink-0 place-content-center rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:bg-primary data-[checked]:text-primary-foreground',
+      'peer grid h-4 w-4 shrink-0 place-content-center rounded-none border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:bg-primary data-[checked]:text-primary-foreground',
       className,
     )}
     {...props}
